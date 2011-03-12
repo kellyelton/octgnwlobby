244bbe34d8e416324ca9f600902f07a61f055c09
Kelly Elton <kelly.elton@skylabsonline.com>
2011-03-12 07:36:58 +0000

    official 1.0.1.13

--- octgnFX/Octgn/Properties/Settings.Designer.cs
+++ octgnFX/Octgn/Properties/Settings.Designer.cs
@@ -133,7 +133,7 @@ namespace Octgn.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.0.1.12")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.0.1.13")]
         public string currevision {
             get {
                 return ((string)(this["currevision"]));
